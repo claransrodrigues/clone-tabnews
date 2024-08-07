b066cb20721ddb1aa6386c76a4b3f164f7dbc145
Manu <110567300+claransrodrigues@users.noreply.github.com>
2024-08-07 17:19:16 +0000

    Adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home (){ // Use an initial capital letter for function names - Convention for React components
+    return <h1>Initial Page</h1>
+}
+
+export default Home; //Indicate to next.js the default page. Next updates automatically
