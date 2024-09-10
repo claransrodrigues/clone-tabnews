528a496862cf09269ac0cf5e70da167e221a83e8
Manu <110567300+claransrodrigues@users.noreply.github.com>
2024-09-10 21:03:31 +0000

    Alteração do texto inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){ // Use an initial capital letter for function names - Convention for React components
-    return <h1>Initial Page</h1>
+    return <h1>Hello World</h1>
 }
 
 export default Home; //Indicate to next.js the default page. Next updates automaticallygit push
