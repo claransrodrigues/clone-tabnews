468dbd4293c0cf4d2c8c087b2a92ed352ec31f29
Manu <110567300+claransrodrigues@users.noreply.github.com>
2024-09-24 20:59:48 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-function Home (){ // Use an initial capital letter for function names - Convention for React components
-    return <h1>Hello World</h1>
+function Home() {
+  // Use an initial capital letter for function names - Convention for React components
+  return <h1>Hello World</h1>;
 }
 
 export default Home; //Indicate to next.js the default page. Next updates automaticallygit push
